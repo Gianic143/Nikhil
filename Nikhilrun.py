8810585344b4e4b934ac071e8a997ff71843d2ab
Gianic143 <103998070+Gianic143@users.noreply.github.com>
2022-05-04 20:18:03 +0530

    Create Nikhilrun.py

---
+++ Nikhilrun.py
@@ -0,0 +1,3 @@
+git clone https://github.com/Gianic143/Nikhil /root/userbot && mkdir /root/userbot/bin/ && cd /root/userbot/ && chmod +x /usr/local/bin/* && python3 -m userbot")
+
+© 2022 GitHub, Inc.TermsPrivacySecurityStatusDocs
